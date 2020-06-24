b06015f8f431578c5e15f49cfbadafc560d34312
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-06-24 16:23:47 -0500

    add echo

--- perf/profiles/jmeter/prestop.sh
+++ perf/profiles/jmeter/prestop.sh
@@ -3,4 +3,5 @@
 test -f /opt/out/instance/bin/jmeter-csv.csv && csv=$(cat /opt/out/instance/bin/jmeter-csv.csv)
 
 echo "$csv"
+echo "TRYING TO SEND RESULTS"
 curl -X POST -H 'Content-type: application/json' --data '{"text":"'"${csv}"'"}' https://hooks.slack.com/services/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
