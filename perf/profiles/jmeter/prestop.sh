38fe19a46a237146db76614769607e8b0e0c8e9f
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-06-24 16:15:21 -0500

    jmeter export csv prestop

---
+++ perf/profiles/jmeter/prestop.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+test -f /opt/out/instance/bin/jmeter-csv.csv && csv=$(cat /opt/out/instance/bin/jmeter-csv.csv)
+
+echo "$csv"
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'"${csv}"'"}' https://hooks.slack.com/services/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
